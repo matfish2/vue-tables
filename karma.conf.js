c74d7bb83c1a6adc0f9d370890db404f1d9796de
Matanya Fishaimer <matfish2@gmail.com>
2015-12-04 22:21:11 +0200

    fix karma config

--- karma.conf.js
+++ karma.conf.js
@@ -63,11 +63,6 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome','Firefox'],
 
-    browserify: {
-        transform: ['stringify']
-    },
-
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
